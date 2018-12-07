ff924da35506ed13bbd8f0f1e42a184db42086fe
Raluca B <ralucaartene@yahoo.com>
2018-12-07 14:05:31 +0200

    DatePicker update
    Start date picker implemented

--- app/src/main/java/ro/umfcd/stud/arteneraluca/myapplication/AlarmSet.java
+++ app/src/main/java/ro/umfcd/stud/arteneraluca/myapplication/AlarmSet.java
@@ -1,18 +1,25 @@
 package ro.umfcd.stud.arteneraluca.myapplication;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class AlarmSet extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class AlarmSet extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     Context m_context;
     View m_view;
 
@@ -22,6 +29,8 @@ public class AlarmSet extends AppCompatActivity {
         setContentView(R.layout.activity_alarm_set);
         m_context = this;
         m_view = findViewById(android.R.id.content);
+        Calendar calendar = Calendar.getInstance();
+
         Switch alarmDuration_switch = (Switch) findViewById(R.id.alarmDuration_switch);
         final TextView alarmsNumber_Text = (TextView) findViewById(R.id.alarmsNumber_Text);
         final Spinner treatmentLengthSpinner = (Spinner) findViewById(R.id.treatmentLengthSpinner);
@@ -40,6 +49,31 @@ public class AlarmSet extends AppCompatActivity {
                     }
                 }
             });
+
+
+        TextView alarm_startDate = (TextView) findViewById(R.id.startDateSelection);
+        String currentDateString = DateFormat.getDateInstance().format(calendar.getTime());
+        alarm_startDate.setText(currentDateString);
+
+        alarm_startDate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DialogFragment datePicker = new DatePickerFragment();
+                    datePicker.show(getSupportFragmentManager(),"date picker");
+
+                }
+            });
+
+
+
+
+
+
+
+
+
+
+
         Button saveAlarm = (Button) findViewById(R.id.alarmSetButton);
         saveAlarm.setOnClickListener(new View.OnClickListener()
         {
@@ -49,6 +83,18 @@ public class AlarmSet extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onDateSet (DatePicker view , int year, int month, int dayOfMonth){
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String setDateString = DateFormat.getDateInstance().format(c.getTime());
+        TextView alarm_startDate = (TextView) findViewById(R.id.startDateSelection);
+        alarm_startDate.setText(setDateString);
+    }
+
 
     private void TrySaveAlarm()
     {
---
+++ app/src/main/java/ro/umfcd/stud/arteneraluca/myapplication/DatePickerFragment.java
@@ -0,0 +1,26 @@
+package ro.umfcd.stud.arteneraluca.myapplication;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment
+{
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar currentTimeDate = Calendar.getInstance();
+        int year = currentTimeDate.get(Calendar.YEAR);
+        int month = currentTimeDate.get(Calendar.MONTH);
+        int day = currentTimeDate.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), 0,(DatePickerDialog.OnDateSetListener) getActivity(),
+                year, month,day);
+
+    }
+}
