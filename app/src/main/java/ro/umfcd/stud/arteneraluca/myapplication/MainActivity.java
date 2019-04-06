e9f972b907199449acb2162f5e396143b7112356
Raluca B <ralucaartene@yahoo.com>
2019-04-06 18:42:26 +0300

    Renamed MasterPage

--- app/src/main/java/ro/umfcd/stud/arteneraluca/myapplication/MainActivity.java
+++ app/src/main/java/ro/umfcd/stud/arteneraluca/myapplication/MainActivity.java
@@ -2,10 +2,7 @@ package ro.umfcd.stud.arteneraluca.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -33,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent toMainPage =  new Intent("android.intent.action.DIALOG");
-                toMainPage.setClass(context, MasterPage.class);
+                toMainPage.setClass(context, ReportPage.class);
                 toMainPage.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(toMainPage);
             }
